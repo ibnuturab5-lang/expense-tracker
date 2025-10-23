b53e18eeb17e82230e2bf4488ada7b5eb2363f22
ibnuturab5-lang <ibnuturab5@gmail.com>
2025-10-23 05:14:48 +0000

    env variable updated

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-
+const API_BASE_URL="https://expense-tracker-eu7d.onrender.com" || "https://friendly-adventure-5g796v74x6g7hvgg-8080.app.github.dev"
 const axiosInstance = axios.create({
-  baseURL: "https://friendly-adventure-5g796v74x6g7hvgg-8080.app.github.dev",
+  baseURL: API_BASE_URL,
   withCredentials: true,
 });
 axiosInstance.interceptors.request.use(
